ae3df65161ad018cfa7b6c87ccb11f8d2d5ad600
user <email>
2021-11-17 14:59:05 +0100

    fixed dockerfile - volume had wrong permissions - could not clone git repo

--- docker/02-docker-run.sh
+++ docker/02-docker-run.sh
@@ -4,14 +4,15 @@
 docker stop opends_main
 docker rm opends_main
 docker container create -it -v opends_data:/data --name opends_main opends
+#docker container create -it --name opends_main opends
 
 #RELPATH=".."
 #cd RELPATH
-cd ..
+#cd ..
 #docker cp shared/* opends_main:/data
 
 #for i in *.sh; do echo $i ; done
-for i in *.sh; do docker cp $i opends_main:/data ; done
+#for i in *.sh; do docker cp $i opends_main:/data ; done
 
 #docker cp compile-hyleap.sh opends_main:/data
 #docker cp compile-IS-DESPOT-p.sh opends_main:/data
@@ -20,8 +21,9 @@ for i in *.sh; do docker cp $i opends_main:/data ; done
 #docker cp run-IS-DESPOT-p.sh opends_main:/data
 #docker cp run-nava3c-p.sh opends_main:/data
 
-docker cp ISDESPOT opends_main:/data
-docker cp Python   opends_main:/data
+#docker cp ISDESPOT opends_main:/data
+#docker cp Python   opends_main:/data
 
 docker start opends_main
 
+docker exec opends_main git clone https://github.com/9f9f/cts.git .
--- docker/w0-docker-init.bat
+++ docker/w0-docker-init.bat
@@ -10,10 +10,7 @@ docker stop opends_main
 docker rm opends_main
 docker container create -it -v opends_data:/data --name opends_main opends
 
-cd ..
-
 docker start opends_main
 
-docker exec --rm --volumes-from opends_main -v $(pwd)/..:/backup opends_main bash -c "cd /data && tar xvf /backup/external.tar --strip 1"
-
+docker exec opends_main git clone https://github.com/9f9f/cts.git .
 
